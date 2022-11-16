c6206833b66bbc6e591b71367622317c6588e162
Aaron <acm19@outlook.com>
2022-11-16 16:04:22 -0500

    first commit

---
+++ MVVMCalculator/MVVMCalculator/ViewModels/SimpleCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace MVVMCalculator.ViewModels
+{
+    public static class SimpleCalculator
+    {
+        public static double Calculate(double value1, double value2, string mathOperator)
+        {
+            double result = 0;
+
+            switch (mathOperator)
+            {
+                case "÷":
+                    result = value1 / value2;
+                    break;
+                case "×":
+                    result = value1 * value2;
+                    break;
+                case "+":
+                    result = value1 + value2;
+                    break;
+                case "-":
+                    result = value1 - value2;
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
+
